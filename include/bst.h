4c28f057996721e7306bb36d5c23a3ffc2d5c24c
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-05-30 00:19:18 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,110 +1,68 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-#include <string>
-#include <iostream>
-template <typename t>
-struct node {
-    node<t>* right = nullptr;
-    node<t>* left = nullptr;
-    node<t>* prev = nullptr;
-    int cnt = 0;
-    t value = "";
-};
-template <typename t>
+
+template<typename T>
+
 class BST {
  private:
-    node<t>* root = nullptr;
-    int hght = 0;
-    void getdepth(node<t>* tree, int x) {
-        ++x;
-        if (tree->left != nullptr) {
-            getdepth(tree->left, x);
-        }
-        if (tree->right != nullptr) {
-            getdepth(tree->right, x);
-        }
-        if (x > hght) {
-            hght = x;
-        }
+  struct Node {
+    T val;
+    Node *right;
+    Node *left;
+    int cnt;
+  };
+  Node *root;
+  Node *addNode(Node *root, T x) {
+      if (root == nullptr) {
+          root = new Node;
+          root->val = x;
+          root->left = nullptr;
+          root->right = nullptr;
+      } else {
+          if (root->val > x) root->left = addNode(root->left, x);
+          if (root->val < x) root->right = addNode(root->right, x);
+          if (root->val == x) (root->cnt)++;
+      }
+      return root;
     }
-    void delnode(node<t>* node) {
-        if (node->left != nullptr) {
-            delnode(node->left);
-        }
-        if (node->right != nullptr) {
-            delnode(node->right);
-        }
-        delete node;
+
+  int searchValue(Node* root, T x) {
+    if (root == nullptr) {
+      return 0;
+    } else if (root->val == x) {
+        return root->cnt;
+    } else if (root->val < x) {
+        return searchValue(root->right, x);
+    } else {
+        return searchValue(root->left, x);
     }
+  }
+
+  int height(Node* root) {
+      if (root == nullptr)
+        return 0;
+      if (root->left == nullptr && root->right == nullptr) return 0;
+      int rrtt = height(root->right), lltt = height(root->left);
+      if (rrtt > lltt)
+        return rrtt + 1;
+      else
+        return lltt + 1;
+  }
+
  public:
-    BST() {
-        root = nullptr;
-        hght = 0;
-    }
-    ~BST() {
-        delnode(root);
-    }
-    void add(t item) {
-        node<t>* cur = root;
-        node<t>* prev = nullptr;
-        int ps = 0;
-        while (true) {
-            if (cur == nullptr) {
-                cur = new node<t>;
-                cur->left = nullptr;
-                cur->right = nullptr;
-                cur->value = item;
-                cur->cnt = 1;
-                if (prev != nullptr) {
-                    cur->prev = prev;
-                    if (ps == 1) {
-                        prev->right = cur;
-                    } else {
-                        prev->left = cur;
-                    }
-                    ps = 0;
-                    prev = nullptr;
-                }
-                if (root == nullptr) {
-                    root = cur;
-                }
-                break;
-            }
-            if (cur->value < item) {
-                prev = cur;
-                ps = 1;
-                cur = cur->right;
-            } else if (cur->value == item) {
-                cur->cnt++;
-                break;
-            } else {
-                prev = cur;
-                ps = -1;
-                cur = cur->left;
-            }
-        }
-    }
-    int search(t item) {
-        node<t>* cur = root;
-        while (true) {
-            if (cur == nullptr) {
-                throw std::string("tree is empty!");
-            } else if (cur->value < item) {
-                cur = cur->right;
-            } else if (cur->value == item) {
-                return cur->cnt;
-            } else {
-                cur = cur->left;
-            }
-        }
-    }
-    int depth() {
-        node<t>* cur = root;
-        getdepth(cur, 0);
-        return hght - 1;
-    }
+  BST():root(nullptr) {}
+  void add(T x) {
+      root = addNode(root, x);
+  }
+  int depth() {
+      return height(root);
+  }
+  int search(T x) {
+      return searchValue(root, x) + 1;
+  }
 };
+
 #endif  // INCLUDE_BST_H_
 
 

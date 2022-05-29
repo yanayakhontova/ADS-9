b8d464c1b6c4a143a6d8a2f3b30312ad6c84de90
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-05-29 23:30:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,20 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+
+    std::ifstream file(filename);
+    BST<std::string> tree;
+    std::string word = "";
+    int count = 0;
+// читаем содержимое файла посимвольно
+    while (!file.eof()) {
+        char ch = file.get();
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            if (ch >= 'A' && ch <= 'Z') ch += 32;
+            word += ch;
+            count++;
+        } else if (count > 0) { tree.add(word); word = "";  count = 0;}
+    }
+    file.close();
+    return tree;
 }

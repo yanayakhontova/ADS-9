6dab1ba2c70af51903bbd5025138d40fef889d68
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-05-29 23:59:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,28 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
-
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include <string>
+#include "bst.h"
 BST<std::string> makeTree(const char* filename) {
-    std::ifstream file(filename);
     BST<std::string> tree;
-    std::string word = "";
-    int count = 0;
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::string("file not found!");
+    }
+    std::string slovo = "";
     while (!file.eof()) {
-        char ch = file.get();
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-            if (ch >= 'A' && ch <= 'Z') ch += 32;
-            word += ch;
-            count++;
-        } else if (count > 0) { tree.add(word); word = "";  count = 0;}
+        char chr = file.get();
+        int sim = chr;
+        if ((sim >= 65 && sim <= 90) || (sim >= 97 && sim <= 122)) {
+            slovo += tolower(sim);
+        } else {
+            if (slovo != "") {
+                tree.add(slovo);
+                slovo = "";
+            }
+        }
     }
     file.close();
     return tree;

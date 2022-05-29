3cb2a3b1cec3a490804601ad37032b130a58f9d5
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-05-29 23:40:39 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -11,51 +11,52 @@ class BST {
     int count;
   };
   Node *root;
-  Node *addNode(Node *root, T q) {
+  Node *addNode(Node *root, T x) {
       if (root == nullptr) {
           root = new Node;
-          root->val = q;
+          root->val = x;
           root->lft = nullptr;
           root->rght = nullptr;
       } else {
-          if (root->val > q) root->lft = addNode(root->lft, q);
-          if (root->val < q) root->rght = addNode(root->rght, q);
-          if (root->val == q) (root->count)++;
+          if (root->val > x) root->lft = addNode(root->lft, x);
+          if (root->val < x) root->rght = addNode(root->rght, x);
+          if (root->val == x) (root->count)++;
       }
       return root;
     }
-  int searchValue(Node* root, T q) {
+  int searchValue(Node* root, T x) {
     if (root == nullptr) {
       return 0;
-    } else if (root->val == q) {
+    } else if (root->val == x) {
         return root->count;
-    } else if (root->val < q) {
-        return searchValue(root->rght, q);
+    } else if (root->val < x) {
+        return searchValue(root->rght, x);
     } else {
-        return searchValue(root->lft, q);
+        return searchValue(root->lft, x);
     }
   }
   int hght(Node* root) {
       if (root == nullptr)
         return 0;
       if (root->lft == nullptr && root->rght == nullptr) return 0;
-      int rrt = hght(root->rght), lt = hght(root->lft);
-      if (rrt > lt)
-        return rrt + 1;
+      int rt = hght(root->rght), lt = hght(root->lft);
+      if (rt > lt)
+        return rt + 1;
       else
         return lt + 1;
   }
  public:
   BST():root(nullptr) {}
-  void add(T q) {
-      root = addNode(root, q);
+  void add(T x) {
+      root = addNode(root, x);
   }
   int depth() {
       return hght(root);
   }
-  int search(T q) {
-      return searchValue(root, q) + 1;
+  int search(T x) {
+      return searchValue(root, x) + 1;
   }
 };
 
 #endif  // INCLUDE_BST_H_
+
